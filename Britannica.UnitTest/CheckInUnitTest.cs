41521400bed6784fcc3b540f401a8d8cb9b893fe
Avi Nissimisn <nessimian.avi@gmail.com>
2021-08-23 17:26:06 +0300

    Addint CheckInUnitTests

---
+++ Britannica.UnitTest/CheckInUnitTest.cs
@@ -0,0 +1,87 @@
+using Britannica.Application.Contracts;
+using Britannica.Application.Exceptions;
+using Britannica.Application.Interactors;
+using Britannica.Domain.Entities;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Britannica.UnitTest
+{
+    public class CheckInUnitTest
+    {
+        public void CheckInInteractor_ValidateAircraftWeightLimit_ThrowsBusinessRuleException()
+        {
+            //arrange
+            var cToken = CancellationToken.None;
+
+            var logger = new NullLogger<CheckInInteractor>();
+            var aircraftRepositoryMock = Mock.Of<IAircraftRepository>();
+            var passengerRepositoryMock = Mock.Of<IPassengerRepository>();
+
+            var flightRepositoryMock = new Mock<IFlightRepository>();
+            var flight = Task.FromResult(new FlightEntity
+            {
+                Id = 1,
+                Aircraft = new AircraftEntity
+                {
+                    WeightLimit = 12
+                },
+                PassengerFlights = new List<PassengerFlightEntity>()
+                {
+                    new PassengerFlightEntity
+                    {
+                        Baggages = new List<BaggageEntity>
+                        {
+                            new BaggageEntity
+                            {
+                                Weight = 6
+                            },
+                            new BaggageEntity
+                            {
+                                Weight = 6
+                            }
+                        }
+                    }
+                }
+            });
+
+            flightRepositoryMock
+                .Setup(m => m.Get(1, cToken))
+                .Returns(flight);
+
+            var interactor = new CheckInInteractor(
+                logger,
+                flightRepositoryMock.Object,
+                aircraftRepositoryMock,
+                passengerRepositoryMock);
+
+            var request = new CheckInRequest
+            {
+                FlightId = 1,
+                PassengerId = 1,
+                Baggages = new List<CheckInRequest.Baggage>
+                {
+                    new CheckInRequest.Baggage
+                    {
+                        Weight = 1
+                    }
+                },
+            };
+
+            //act
+            Action act = () => interactor.Handle(request, cToken).GetAwaiter().GetResult();
+
+            //assert
+            var exception = Assert.Throws<BusinessRuleException>(act);
+
+            //More detailed assertions.
+            Assert.Equal($"Aircraft has a limited load weight of {flight.Result.Aircraft.WeightLimit}", exception.Message);
+
+        }
+    }
+}
